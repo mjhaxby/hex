83ffaf6db113c10516db1cf805023c99903ccf12
mjh-source <morgan@yellow-1.home>
2023-07-08 09:20:47 +0200

    decisions activity, scorm support for match and side by side mode, falling words multiple choice mode, other small changes for v3.0

--- Activities/activityController.js
+++ Activities/activityController.js
@@ -1,10 +1,10 @@
-const { ipcRenderer } = require('electron');
+// The ipcRenderer module provides a few methods so you can send events from the render process (web page) to the main process.
+// It is now enabled in the preload.js file.
 
 var gameData;
 var gameSettings;
 var filename = window.location.pathname.substring(window.location.pathname.lastIndexOf('/')+1);
 var activityname = filename.match(/.*(?=\.)/)[0]
-
 
 ipcRenderer.on('loadActivity', (event, data, settings) => {
   console.log(data)
